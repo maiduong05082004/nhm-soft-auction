266b3af215c8748201b3a9007bd0f7fcba41f057
maiduong <duongmdph40323@gmail.com>
2025-08-11 17:26:49 +0700

    Edit Manager Order

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -6,6 +6,7 @@ use App\Utils\HelperFunc;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Product;
 class OrderDetail extends Model
 {
     use HasFactory;
@@ -19,11 +20,27 @@ class OrderDetail extends Model
         'total',
     ];
 
+    protected $casts = [
+        'quantity' => 'float',
+        'total' => 'float',
+    ];
+
     protected static function boot()
     {
         parent::boot();
         static::creating(function ($model) {
             $model->id = HelperFunc::getTimestampAsId();
+        });
+
+        static::saving(function (OrderDetail $model) {
+            $price = 0.0;
+            if ($model->relationLoaded('product') && $model->product) {
+                $price = (float) $model->product->price;
+            } else if (! empty($model->product_id)) {
+                $price = (float) (Product::find($model->product_id)?->price ?? 0);
+            }
+            $quantity = (float) ($model->quantity ?? 0);
+            $model->total = $quantity * $price;
         });
     }
 
