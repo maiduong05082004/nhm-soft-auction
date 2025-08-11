5ee6b7114a3e10f9827f7ecedaf64cb7ffdd2a88
truonghocdot05 <truonghocdot05@gmail.com>
2025-08-11 15:49:37 +0700

    truonghd - remove vue

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('components.layouts.app')
+
+@section('content')
+    <div >
+        <button class="px-4 py-2 bg-blue-500 text-white rounded">
+            Toggle Message
+        </button>
+        <div class="mt-4 p-4 bg-green-100 rounded">
+            Xin chào từ AlpineJS!
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,19 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Đây là nơi đăng ký các route web cho ứng dụng của bạn.
+| Các route này sẽ load qua RouteServiceProvider và nằm trong group "web".
 |
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return view('welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
@@ -30,6 +28,6 @@ Route::middleware([
     'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
+        return view('dashboard');
     })->name('dashboard');
 });
