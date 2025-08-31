93cedbf0f249deaaab1bbc115cc40e380d1f3373
Mai Văn Trường <160713748+Truonghocdot@users.noreply.github.com>
2025-08-31 22:24:36 +0700

    Update UserResource.php

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -101,7 +101,7 @@ class UserResource extends Resource
 
     public static function table(Table $table): Table
     {
-        return $table->modifyQueryUsing(fn(Builder $query) => $query->with('author', 'category'))
+        return $table->modifyQueryUsing(fn(Builder $query) => $query->with( 'category'))
             ->columns([
                 Tables\Columns\TextColumn::make('name')
                     ->label('Tên')
